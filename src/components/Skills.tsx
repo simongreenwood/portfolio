1810d95455a76af7371e123492be57e8480f324b
simon greenwood <simonrobertgreenwood@gmail.com>
2024-04-22 21:14:47 +0100

    updated the skills section again

--- src/components/Skills.tsx
+++ src/components/Skills.tsx
@@ -4,48 +4,18 @@ export default function Skills() {
   return (
     <div className="">
       <h3 className="text-2xl pt-8 font-semibold">skills</h3>
-      <p className="pb-2">here are some of my favourite technologies to use</p>
-
+      <p className="pb-2">
+        here are the main technologies i use when making a project, although i'm
+        flexible and know others
+      </p>
       <div className="bg-slate-900 border border-gray-600 rounded-lg p-4 text-base">
         <ul className="space-y-4">
-          <li>
-            <h4 className="text-xl font-semibold">frontend</h4>
-            <p>
-              - <span className="text-amber-300">react</span>,{" "}
-              <span className="text-amber-300">nextjs</span>,{" "}
-              <span className="text-amber-300">apollo</span>/
-              <span className="text-amber-300">graphql</span>
-            </p>
-          </li>
-          <li>
-            <h4 className="text-xl font-semibold">backend</h4>
-            <p>
-              - <span className="text-amber-300">prisma</span>,{" "}
-              <span className="text-amber-300">express</span>,{" "}
-              <span className="text-amber-300">postgresql</span>,{" "}
-              <span className="text-amber-300">mongodb</span>,{" "}
-              <span className="text-amber-300">redis</span>
-            </p>
-          </li>
-          <li>
-            <h4 className="text-xl font-semibold">languages</h4>
-            <p>
-              - <span className="text-amber-300">javascript/nodejs</span>,{" "}
-              <span className="text-amber-300">typescript</span>,{" "}
-              <span className="text-amber-300">python</span>
-            </p>
-          </li>
-          <li>
-            <h4 className="text-xl font-semibold">other</h4>
-            <p>
-              - <span className="text-amber-300">git</span>,{" "}
-              <span className="text-amber-300">docker</span>,{" "}
-              <span className="text-amber-300">github actions</span>,{" "}
-              <span className="text-amber-300">linux</span>,{" "}
-              <span className="text-amber-300">aws</span>,{" "}
-              <span className="text-amber-300">vercel</span>
-            </p>
-          </li>
+          <li className="pl-2">- Typescript</li>
+          <li className="pl-2">- Python</li>
+          <li className="pl-2">- Next.js/React</li>
+          <li className="pl-2">- Tailwind CSS</li>
+          <li className="pl-2">- Docker</li>
+          <li className="pl-2">- PostgreSQL</li>
         </ul>
       </div>
     </div>
